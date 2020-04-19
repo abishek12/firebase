14dc8a113c1df7919be83a4671a22752af4a2384
abishek <abishekkhanal999@gmail.com>
2020-04-19 15:44:19 +0545

    V-1

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,22 +1,76 @@
 package com.example.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView actSignup;
+    EditText logUsername,logPassword;
+    String strLogUsername, strLogPassword;
+    Button buttonLogin;
+
+    FirebaseAuth lAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lAuth = FirebaseAuth.getInstance();
+
         actSignup = findViewById(R.id.actSignup);
+        logUsername = findViewById(R.id.username);
+        logPassword = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.btnLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                strLogUsername = logUsername.getText().toString().trim();
+                strLogPassword = logPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(strLogUsername)){
+                    logUsername.setError("Required Field");
+                    return;
+                }
+                if (TextUtils.isEmpty(strLogPassword)){
+                    logUsername.setError("Required Field");
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(strLogUsername).matches()){
+                    logUsername.setError("Enter valid email address");
+                    return;
+                }
+                lAuth.signInWithEmailAndPassword(strLogUsername,strLogPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Login",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"unsuccessfull",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
         actSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/firebase/SignupActivity.java
+++ app/src/main/java/com/example/firebase/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,33 +9,34 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText regUsername, regEmail, regNumber, regPassword, regConfirmPassword;
-    Button buttonRegister;
-    String strEmail,strPassword,strConfirmPassword;
+    private EditText regUsername, regEmail, regNumber, regPassword, regConfirmPassword;
+    private Button buttonRegister;
+    private String strEmail, strPassword, strConfirmPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        findId();
-        registration();
-
-    }
-
-    private void findId(){
+        mAuth = FirebaseAuth.getInstance();
         regUsername = findViewById(R.id.regUsername);
         regEmail = findViewById(R.id.regEmail);
         regNumber = findViewById(R.id.regNumber);
         regPassword = findViewById(R.id.regPassword);
         regConfirmPassword = findViewById(R.id.regConfirmPassword);
         buttonRegister = findViewById(R.id.buttonRegister);
-    }
 
-    private void registration(){
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,26 +46,37 @@ public class SignupActivity extends AppCompatActivity {
 
 //                form validation
 
-                if (TextUtils.isEmpty(strEmail)){
+                if (TextUtils.isEmpty(strEmail)) {
                     regEmail.setError("Required field...");
+                    return;
                 }
-                if (!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()){
+                if (!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
                     regEmail.setError("Enter valid email address");
+                    return;
                 }
-                if (strPassword.length()<=7 || strPassword.length() >=16){
+                if (strPassword.length() <= 7 || strPassword.length() >= 16) {
                     regPassword.setError("Password should be less than 16 or greater than 7");
+                    return;
                 }
-                if (TextUtils.isEmpty(strPassword)){
+                if (TextUtils.isEmpty(strPassword)) {
                     regPassword.setError("Required field...");
+                    return;
                 }
-                if (TextUtils.isEmpty(strConfirmPassword)){
+                if (TextUtils.isEmpty(strConfirmPassword)) {
                     regConfirmPassword.setError("Required field...");
+                    return;
                 }
-                if (strPassword != strConfirmPassword){
-                    regPassword.setError("Password did not matched");
-                    regConfirmPassword.setText("");
-                }
+
+                mAuth.createUserWithEmailAndPassword(strEmail,strPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
     }
+
 }
